7c51283bb872b617b34a82eb1ce548921cf5b9c6
Sherin Farhana <s.sherinfarhana@gmail.com>
2024-01-14 10:59:22 +0300

    Upgrade to V16

--- odoo_amount_in_words/__manifest__.py
+++ odoo_amount_in_words/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Display Amount In Words in quotations, invoices and purchase orders',
-    'version': '15.0.0.0',
+    'version': '16.0.0.0',
     'author': 'Cloudroits',
     'summary': 'Display Amount In Words in quotations, invoices and purchase orders, both in forms as well as reports',
     'description': """This module displays Total Amount In Words in quotations, invoices and purchase orders, both in forms as well as reports.""",
