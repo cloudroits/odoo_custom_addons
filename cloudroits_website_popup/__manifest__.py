95d9e67f13ba3a85ec01e5fb0723148f47bd7943
inspire-academy <it-support@inspire.qa>
2024-08-14 20:35:25 +0300

    Website Popup Manager

---
+++ cloudroits_website_popup/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Popup Manager',
+    'version': '13.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Easily Create and Manage Popups Across Your Odoo Website',
+    'author': 'Cloudroits',
+    'depends': ['website'],
+    'data': [        
+        'security/website_popup_security.xml',  # Security group
+        'security/ir.model.access.csv', 
+        'views/popup_image_view.xml',
+        'views/website_popup_image.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': False,
+}
